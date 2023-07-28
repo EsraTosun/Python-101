ba3c7c7de82a03a8c68c16a23cfad731476c21f7
EsraTosun <esratosun171@gmail.com>
2023-07-28 23:24:24 +0300

    Update Commit

---
+++ 5-python_fonksiyonlar/1-methods.py
@@ -0,0 +1,20 @@
+
+# method
+# sınıf içerisinde hazır kullanılan yapılardır
+
+list = [1,2,3]
+
+list.append(4)
+list.pop()
+
+print(type(list))
+print(list)
+
+myString = 'Hello'
+
+print(myString.upper())
+
+
+print(type(myString))
+
+# fonksiyon

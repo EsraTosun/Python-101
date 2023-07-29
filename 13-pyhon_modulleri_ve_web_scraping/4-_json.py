6d55461dd544db1408b584e62130f0de9ad1e1ca
EsraTosun <esratosun171@gmail.com>
2023-07-30 02:40:30 +0300

    Update Commit

--- 13-pyhon_modulleri_ve_web_scraping/4-_json.py
+++ 13-pyhon_modulleri_ve_web_scraping/4-_json.py
@@ -1,13 +1,16 @@
 import json
+# cihazlar arasında ortak bir veri taşıma
 
 person_string = '{"name":"Ali", "languages":["python","C#"]}'
 person_dict = {"name": "Ali","languages": ["Python","C#"] }
 
 # JSON string to Dict
-# result = json.loads(person_string)
+# string ifadeyi dict yapar
+# result = json.loads(person_string)  
 # result = result["name"]
 # result = result["languages"]
 
+# Dosyadan okuma
 # with open("person.json") as f:
 #     data = json.load(f)
 #     print(data["name"])
@@ -18,11 +21,15 @@ person_dict = {"name": "Ali","languages": ["Python","C#"] }
 # result = json.dumps(person_dict)
 # print(type(result))
 
+# Dosya yazma
 # with open("person.json","w") as f:
 #     json.dump(person_dict, f)
 
 # person_dict = json.loads(person_string)
+# syring => dict dönüştürülür
 
 # result = json.dumps(person_dict, indent= 4, sort_keys= True)
+# ekrana düzgün yazılması için yapılır
+# indent => kaç karakterlij boşluk bıraksın
 # print(person_dict)
 # print(result)

20a48ec5e73652004c5ed9b6cb34241545f9ce65
EsraTosun <esratosun171@gmail.com>
2023-08-06 00:46:59 +0300

    update commit

---
+++ 14-python_ile_bot_yazimi/2-selenium-basics.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time   # ziyaret ettiğimiz sayfada durmamızı sağlar
+
+driver = webdriver.Chrome()
+
+url = "http://github.com"
+driver.get(url)   # girdiğimiz url ziyaret eder
+
+time.sleep(2)    # url de 2 sn durur
+driver.maximize_window()   # yüklendikten 2 sn sonra tam ekran olur
+driver.save_screenshot("github.com-homepage.png")
+# Açtığı sayfanın resmini çeker
+
+url = "http://github.com/EsraTosun"
+driver.get(url)
+
+print(driver.title)
+
+if "EsraTosun" in driver.title:
+     driver.save_screenshot("github-EsraTosun.png")
+
+time.sleep(2)
+
+driver.back()   # sayfayı geri alır
+# driver.forward()  # sayfayı ileri alır
+
+time.sleep(2)
+
+driver.close()

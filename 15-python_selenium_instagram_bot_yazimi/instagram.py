6df8e823817835999e6e956a67b37e111f496175
EsraTosun <esratosun171@gmail.com>
2023-08-17 22:08:55 +0300

    Update Commit

--- 15-python_selenium_instagram_bot_yazimi/instagram.py
+++ 15-python_selenium_instagram_bot_yazimi/instagram.py
@@ -1,13 +1,15 @@
 from instagramUserInfo import username, password
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By   # sayfada istediğimiz bölmü bulmamızı sağlar
 import time
 
 class Instagram:
     def __init__(self,username,password):
+        self.browser = webdriver.Chrome()
         self.browserProfile = webdriver.ChromeOptions()
         self.browserProfile.add_experimental_option('prefs', {'intl.accept_languages':'en,en_US'})
-        self.browser = webdriver.Chrome('chromedriver.exe', chrome_options=self.browserProfile)
+        #self.browser = webdriver.Chrome('chromedriver.exe', chrome_options=self.browserProfile)
         self.username = username
         self.password = password
 
@@ -15,8 +17,8 @@ class Instagram:
         self.browser.get("https://www.instagram.com/accounts/login/")
         time.sleep(3)
         
-        usernameInput = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input")
-        passwordInput = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input")
+        usernameInput = self.browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/div/label/input")
+        passwordInput = self.browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]/div/label/input")
 
         usernameInput.send_keys(self.username)
         passwordInput.send_keys(self.password)
@@ -26,7 +28,7 @@ class Instagram:
     def getFollowers(self, max):
         self.browser.get(f"https://www.instagram.com/{self.username}")
         time.sleep(2)
-        self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a").click()
+        #self.browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a").click()
         time.sleep(2)
         
         dialog = self.browser.find_element_by_css_selector("div[role=dialog] ul")

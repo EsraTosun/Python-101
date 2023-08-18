8936893c20b0e00c6bee4f4b54bab972a0a4393c
EsraTosun <esratosun171@gmail.com>
2023-08-19 00:07:49 +0300

    Update Commit

---
+++ 17-python_veri_analizi_numpy/2-numpy-arrays.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+result = np.array([1,3,5,7,9])
+# result = np.arange(1,10)  # 1 dahil 10 dahil olmayack şeklide list oluşturur
+# result = np.arange(10,100,3)
+# result = np.zeros(10)    # sadece 0 lardan oluşan list
+# result = np.ones(10)     # sadece 1 lardan oluşan list
+# result = np.linspace(0,100,5)  # eşit parçaya bölerek list 
+# result = np.linspace(0,5,5)
+# result = np.random.randint(0,10)
+# result = np.random.randint(20)
+# result = np.random.randint(1,10,3)
+# result = np.random.rand(5)    # - değerlerde dahil
+# result = np.random.randn(5)
+# np_array = np.arange(50)
+# np_multi = np_array.reshape(5,10)
+# print(np_multi.sum(axis=1))
+# print(np_multi.sum(axis=0))
+
+# rnd_numbers = np.random.randint(1,100,10)
+# print(rnd_numbers)
+# result = rnd_numbers.max()
+# result = rnd_numbers.min()
+# result = rnd_numbers.mean()    #ortalama
+# result = rnd_numbers.argmax()   #üretilen en büyük sayı index
+# result = rnd_numbers.argmin()   #üretilen en küçük sayı index
+
+print(result)

2e96b9741fe26a9af9212a41cb85816d5b3cfcd1
EsraTosun <esratosun171@gmail.com>
2023-08-22 16:58:59 +0300

    update commit

--- 18-python_pandas/9-pandas-str.py
+++ 18-python_pandas/9-pandas-str.py
@@ -2,14 +2,16 @@ import pandas as pd
 
 data = pd.read_csv("datasets/nba.csv")
 
-data.dropna(inplace = True)
+data.dropna(inplace = True)   
+# inplace => orjinal data üzerinden değişiklik yapmamızı sağlar
 
-# data["Name"] = data["Name"].str.upper()
-# data["Name"] = data["Name"].str.lower()
-# data["index"] = data["Name"].str.find('a')
-# data = data[data.Name.str.contains('Jordan')]
-# data = data.Team.str.replace(' ','-')
+# data["Name"] = data["Name"].str.upper()   # Hepsini büyük harf yapar
+# data["Name"] = data["Name"].str.lower()   # küçük harf
+# data["index"] = data["Name"].str.find('a')    # a nın bulunduğu index
+# data = data[data.Name.str.contains('Jordan')]    # Jordan bulunan iadeyi bulur
+# data = data.Team.str.replace(' ','-')   # boşluk yerine - koyar
 data[['FirstName','LastName']] = data['Name'].loc[data['Name'].str.split().str.len()==2].str.split(expand=True)
+# split datayı boşluk ile böler
 
 
 print(data.head(10))

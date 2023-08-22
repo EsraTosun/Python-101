c31d10851b75628575c662064363e8d68a2e5527
EsraTosun <esratosun171@gmail.com>
2023-08-22 16:00:09 +0300

    update commit

--- 18-python_pandas/8-missing-data.py
+++ 18-python_pandas/8-missing-data.py
@@ -11,34 +11,34 @@ newColumn = [np.nan,30,np.nan,51,np.nan,30,np.nan,10]
 df["column4"] = newColumn
 
 result = df
-result = df.drop("column1", axis = 1)
+result = df.drop("column1", axis = 1)     # silme işlemini yapar
 result = df.drop(["column1","column2"], axis = 1)
 result = df.drop('a', axis = 0)
 result = df.drop(['a','b','h'], axis = 0)
 
 result = df.isnull()
 result = df.notnull()
-result = df.isnull().sum()
+result = df.isnull().sum()   #her columdaki null adeti
 result = df["column1"].isnull().sum()
 result = df[df["column1"].isnull()]
 result = df[df["column1"].isnull()]["column1"]
 result = df[df["column1"].notnull()]["column1"]
 
-result = df.dropna() # axis = 0 => satıra
+result = df.dropna() # axis = 0 => satıra   null olan satırları sil
 result = df.dropna(axis = 1) # axis = 1 => sütuna
-result = df.dropna(how = "any")
-result = df.dropna(how = "all")
-result = df.dropna(subset  = ["column1","column2"], how = "all")
+result = df.dropna(how = "any") # her hangi bir null da siler
+result = df.dropna(how = "all") # tüm elemanlar null ise siler
+result = df.dropna(subset  = ["column1","column2"], how = "all")  
 result = df.dropna(subset  = ["column1","column2"], how = "any")
 result = df.dropna(thresh = 2)
 result = df.dropna(thresh = 3) # en az sayıda normal veri
 
-result = df.fillna(value = 'no input')
+result = df.fillna(value = 'no input')  # null olan verileri value yazar
 result = df.fillna(value = 1)
 
-result = df.sum()
-result = df.sum().sum()
-result = df.size
+result = df.sum()   # her column toplam
+result = df.sum().sum()    # tüv verilerin toplamı
+result = df.size    # eleman sayısı
 result = df.isnull().sum()
 result = df.isnull().sum().sum()
 

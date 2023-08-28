f3ba4b5df8893f4a065300a172fea62600a88ded
EsraTosun <esratosun171@gmail.com>
2023-08-29 01:10:59 +0300

    update commit

--- 20-python_SQL_veritabani_MySql/1-mysql-connection.py
+++ 20-python_SQL_veritabani_MySql/1-mysql-connection.py
@@ -1,13 +1,34 @@
-import mysql.connector
+import pymysql.cursors
 
-mydb = mysql.connector.connect(
+mydb = pymysql.connect(
     host = "localhost", # 192.23.45.56
-    user = "root1",
+    user = "root",
     password = "mysql1234",
-    database = "node-app"
-)
+    db = "node-app",
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+    )
 
 mycursor = mydb.cursor()
 
+# mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("DROP DATABASE mydatabase")
+
 
 #Database bağlantısı sağlarız
+
+
+
+# import mysql.connector
+
+# mydb = mysql.connector.connect(
+#     host = "localhost", # 192.23.45.56
+#     user = "root1",
+#     password = "mysql1234",
+#     database = "node-app"
+# )
+
+# mycursor = mydb.cursor()
+
+
+# #Database bağlantısı sağlarız
